3888f41ce0218065f02ce514e8fd30a1b09856b9
Anuradha812789 <anuradhaganji152@gmail.com>
2026-08-05 11:51:35 +0530

    Fixed search and contact button finally

--- script.js
+++ script.js
@@ -1,19 +1,5 @@
-
-function addToCart(name, price) {
-
-  let cart = JSON.parse(localStorage.getItem('shopnova_cart')) || [];
-
-  cart.push({ name, price });
-
-  localStorage.setItem('shopnova_cart', JSON.stringify(cart));
-
-  alert(name + ' added to cart 🛒');
-}
-
 function toggleContactMenu() {
-
   const menu = document.getElementById('contactMenu');
-
   menu.style.display =
     menu.style.display === 'flex' ? 'none' : 'flex';
 }
@@ -21,7 +7,6 @@ function toggleContactMenu() {
 const searchInput = document.getElementById('searchInput');
 
 if (searchInput) {
-
   searchInput.addEventListener('keyup', function () {
 
     const value = this.value.toLowerCase();
@@ -33,8 +18,7 @@ if (searchInput) {
                        .toLowerCase();
 
       card.style.display =
-        name.includes(value) ? 'flex' : 'none';
+        name.includes(value) ? 'block' : 'none';
     });
   });
 }
-
